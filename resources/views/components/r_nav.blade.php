61ac83a2a645c693ecd94af2952f3841c6cc6e5c
dextermontero <dexter.montero09@gmail.com>
2023-10-18 03:50:26 +0800

    Initial Recruiter dashboard layout

--- resources/views/components/r_nav.blade.php
+++ resources/views/components/r_nav.blade.php
@@ -14,7 +14,8 @@
             </div>
                 <div class="flex items-center">
                     <div class="flex items-center ml-3">
-                        <div>
+                        <div class="inline-flex items-center">
+                            <a href="#" class="bg-indigo-600 text-lg px-4 py-1 mr-4 rounded-full text-gray-200 font-poppins font-medium hover:bg-indigo-700 hover:text-gray-300">Create Post</a>
                             <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
                                 <span class="sr-only">Open user menu</span>
                                 @foreach(App\Models\Recruiter::select('recruiter_information.img_profile', 'recruiter_information.firstname', 'recruiter_information.lastname', 'recruiters.email')->join('recruiter_information', 'recruiter_information.uid', '=', 'recruiters.id')->get(['recruiter_information.img_profile', 'recruiter_information.firstname', 'recruiter_information.lastname']); as $recruiter)                              
@@ -34,18 +35,15 @@
                             @endforeach
                         </div>
                         <ul class="py-1" role="none">
-                        <li>
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Dashboard</a>
-                        </li>
-                        <li>
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Settings</a>
-                        </li>
-                        <li>
-                        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Earnings</a>
-                        </li>
-                        <li>
-                        <a href="{{ route('recruiter_logout') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
-                        </li>
+                            <li>
+                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Profile</a>
+                            </li>
+                            <li>
+                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Settings</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('recruiter_logout') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -53,6 +51,61 @@
         </div>
     </div>
 </nav>
+
+<aside id="recruiter-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-gray-900 border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+    <div class="h-full px-3 pb-4 overflow-y-auto mt-8 bg-gray-900 dark:bg-gray-800">
+        <div class="space-y-2 mb-5 flex flex-col items-center justify-center">
+            <img src="{{ asset('assets/job/company_logo/default.png')}}" class="h-24 w-24 shadow-md rounded-lg">
+            <h2 class="text-lg text-gray-200 font-medium font-poppins text-center">Company Name</h2>
+        </div>
+        <ul class="space-y-2 font-medium">
+            <li>
+                <a href="{{ route('recruiter_dashboard') }}" class="flex items-center p-2 text-gray-200 rounded-lg hover:bg-gray-700 group">
+                    <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                        <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
+                        <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+                    </svg>
+                    <span class="ml-3">Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="flex items-center p-2 text-gray-200 rounded-lg hover:bg-gray-700 group">
+                    <i class="fa-solid fa-pencil flex-shrink-0 w-5 h-5 text-lg text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i>
+                    <span class="flex-1 ml-3 whitespace-nowrap">Job Post</span>
+                    <span class="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="flex items-center p-2 text-gray-200 rounded-lg hover:bg-gray-700 group">
+                    <i class="fa-solid fa-building flex-shrink-0 w-5 h-5 text-lg text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i>
+                    <span class="flex-1 ml-3 whitespace-nowrap">Company</span>
+                    <span class="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span>
+                </a>
+            </li>
+            <li>
+                <a href="#" class="flex items-center p-2 text-gray-200 rounded-lg hover:bg-gray-700 group">
+                    <i class="fa-solid fa-gears flex-shrink-0 w-5 h-5 text-lg text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></i>
+                    <span class="flex-1 ml-3 whitespace-nowrap">Settings</span>
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('recruiter_logout') }}" class="flex items-center p-2 text-gray-200 rounded-lg hover:bg-gray-700 group">
+                    <i class="fa-solid fa-right-from-bracket flex-shrink-0 w-5 h-5 text-lg text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-red-600 dark:group-hover:text-white"></i>
+                    <span class="flex-1 ml-3 whitespace-nowrap">Logout</span>
+                </a>
+            </li>
+        </ul>
+    </div>
+</aside>
+
+
+
+
+
+
+
+
+
 @else 
     <nav class="bg-gray-900 py-4 px-1 text-gray-300 z-30">
         <div class="flex flex-wrap justify-between items-center mx-auto container">
@@ -63,7 +116,7 @@
             <div class="flex md:order-2">
                 <ul class="inline-flex items-center font-medium">
                     <li class="mr-4"><a href="/careers" class="w-full hover:text-indigo-800 text-lg">Find jobs</a></li>
-                    <li class="mr-4"><a href="/recruiter/login" class="w-full hover:text-indigo-800 text-lg">Sign In1</a></li>
+                    <li class="mr-4"><a href="/recruiter/login" class="w-full hover:text-indigo-800 text-lg">Sign In</a></li>
                 </ul>
                 <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center mr-2 mt-1 p-4 w-10 h-10 justify-center text-sm text-indigo-600 rounded-lg md:hidden hover:bg-indigo-800 hover:text-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-600 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
---
+++ resources/views/partials/r_footer.blade.php
@@ -0,0 +1,59 @@
+<footer class="mt-auto py-auto relative bottom-0">
+    <div class="w-full">
+        <div class="bg-gray-200 text-gray-800 font-normal">
+            <div class="container mx-auto px-4">
+                <div class="grid xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3">
+                    <div class="mb-3 px-6">
+                        <h2 class="text-2xl font-bold mt-8 text-indigo-800">JobSearch</h2>
+                        <p class="text-sm font-light text-justify mt-3 text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                    <div class="mb-3 px-6">
+                        <h2 class="text-xl font-semibold text-gray-800 mt-8">Explore <span class="font-bold text-indigo-800">JobSearch</span></h2>
+                        <div class="flex flex-wrap sm:mb-0 mb-4 mt-3">
+                            <div class="grid grid-cols-2 gap-10 items-center justify-between">
+                                <div class="mr-5">
+                                    <ul>
+                                        <li><a href="/" class="hover:font-light font-light hover:text-indigo-800 inline-flex items-center"><i class="fa-solid fa-chevron-right text-[12px] mr-1.5 text-blue-700"></i>Explore Career</a></li>
+                                        <li><a href="/careers" class="hover:font-light font-light hover:text-indigo-800 inline-flex items-center"><i class="fa-solid fa-chevron-right text-[12px] mr-1.5 text-blue-700"></i>Explore Jobs</a></li>
+                                        <li><a href="/companies" class="hover:font-light font-light hover:text-indigo-800 inline-flex items-center"><i class="fa-solid fa-chevron-right text-[12px] mr-1.5 text-blue-700"></i>Explore Companies</a></li>
+                                    </ul>
+                                </div>
+                                <div class="mr-5">
+                                    <ul>
+                                        <li><a href="/companies/salaries" class="hover:font-light font-light hover:text-indigo-800 inline-flex items-center"><i class="fa-solid fa-chevron-right text-[12px] mr-1.5 text-blue-700"></i>Job Salary</a></li>
+                                        <li><a href="/countries" class="hover:font-light font-light hover:text-indigo-800 inline-flex items-center"><i class="fa-solid fa-chevron-right text-[12px] mr-1.5 text-blue-700"></i>Countries</a></li>
+                                        <li><a href="#downloads" class="hover:font-light font-light hover:text-indigo-800 inline-flex items-center"><i class="fa-solid fa-chevron-right text-[12px] mr-1.5 text-blue-700"></i>Download Apps</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div> 
+                    </div>
+                    <div class="mb-3 px-6">
+                        <h2 class="text-2xl font-semibold text-gray-800 mt-8">Contact Us</h2>
+                        <div class="mt-3">
+                            <ul>
+                                <li><a href="#help" class="hover:font-light font-light hover:text-indigo-800 inline-flex items-center">Support</a></li>
+                                <li><a href="#help" class="hover:font-light font-light hover:text-indigo-800 inline-flex items-center">Help Center</a></li>
+                            </ul>
+                            <div class="mt-3">
+                                <h2 class="font-semibold text-lg">Follow Us</h2>
+                                <div class="inline-flex mt-2">
+                                    <a href="#facebook" class="mr-3 text-blue-700 text-xl"><i class="fa-brands fa-facebook-f"></i></a>
+                                    <a href="#twitter" class="mr-3 text-black  text-xl"><i class="fa-brands fa-x-twitter"></i></a>
+                                    <a href="#instagram" class="mr-3  text-xl text-transparent bg-gradient-to-r bg-clip-text from-orange-400 via-red-500 to-pink-500"><i class="fa-brands fa-instagram"></i></a>
+                                    <a href="#linkedin" class="mr-3 text-blue-700 text-xl"><i class="fa-brands fa-linkedin-in"></i></a>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-3 pt-5 py-4">
+                    <div class="inline-flex items-center">
+                        <p>&copy; <span id="year"></span> <span class="text-blue-800 font-semibold mr-2">JobSearch</p> | <a href="#" class="ml-2 mr-2 hover:text-blue-800">Privacy Center</a> &bullet; <a href="#" class="ml-2 mr-2 hover:text-blue-800">Terms of Service</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
