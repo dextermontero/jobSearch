4df092f0511dfbe356a998b378d58a06100eb6e2
dextermontero <dexter.montero09@gmail.com>
2023-10-27 02:27:34 +0800

    view company initial layout

---
+++ resources/views/recruiter/company/edit.blade.php
@@ -0,0 +1,18 @@
+@include("partials.r_header", [$title])   
+<div class="min-h-screen">
+    <div class="p-4 sm:ml-64">
+        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-[4.5rem]">
+            <div class="rounded">
+                
+                <div class="flex items-center justify-center h-[28rem] mb-4 rounded dark:bg-gray-800">
+                    <p class="text-2xl text-gray-400 dark:text-gray-500">
+                        Edit Company Info 
+                    </p>
+                </div>
+            </div>
+        </div>
+        @include('partials.r_footer')
+    </div>
+</div>
+</body>
+</html>
